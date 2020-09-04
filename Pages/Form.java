39ced743c39ac773782507fd8da003a5252d6bd7
Aisha <ash.hlatshwayo22@gmail.com>
2020-09-04 02:29:59 +0200

    form page - need help on

---
+++ Pages/Form.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertEquals;
+
+public class Form {
+    private static String[] args;
+
+    public static void main(String[] args) {
+        Form.args = args;
+
+        System.setProperty("webdriver.chrome.driver", "/Users/aishia/Downloads/chromedriver_win32");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/form");
+
+        FormPage formPage = new FormPage();
+        formPage.submitForm(driver);
+
+        ConfirmationPage confirmationPage = new ConfirmationPage();
+        confirmationPage.waitForAlertBanner(driver);
+
+        assertEquals("The form was successfully submitted!", confirmationPage.getAlertBanner(driver));
+
+        driver.quit();
+    }
+
+}
